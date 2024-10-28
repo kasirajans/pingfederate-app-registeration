ce8eec2cac30a922732514fa71ddb65ed0ffd1b8
kasiece@hotmail.com <kasiece@hotmail.com>
2024-10-28 16:42:47 -0600

    Update Provider

--- provider.tf
+++ provider.tf
@@ -1,3 +1,27 @@
 # provider "pingfederate" {
 #   insecure_trust_all_tls = var.INSECURE_TRUST_ALL_TLS ? true : false
+# }# provider "pingfederate" {
+#   insecure_trust_all_tls = var.INSECURE_TRUST_ALL_TLS ? true : false
 # }
+
+terraform {
+  required_version = ">=1.4"
+  required_providers {
+    pingfederate = {
+      version = "~> 1.0.0"
+      source  = "pingidentity/pingfederate"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "pingfederate" {
+  username                            = var.PINGFEDERATE_PROVIDER_USERNAME
+  password                            = var.PINGFEDERATE_PROVIDER_PASSWORD
+  https_host                          = var.PINGFEDERATE_PROVIDER_HTTPS_HOST
+  admin_api_path                      = var.PINGFEDERATE_PROVIDER_ADMIN_API_PATH
+  product_version                     = "12.1"
+}
