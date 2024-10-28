5cbd9f77785a239b0cd21f05f9ff3441c723d335
kasiece@hotmail.com <kasiece@hotmail.com>
2024-10-28 16:00:40 -0600

    Update var and local

--- main.tf
+++ main.tf
@@ -1,51 +1,73 @@
-terraform {
-  required_version = ">=1.1"
-  required_providers {
-    pingfederate = {
-      version = "~> 0.13.0"
-      source  = "pingidentity/pingfederate"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
+# Dynamically load the merged JSON file based on the region and environment
+data "local_file" "merged_json" {
+  filename = "${path.module}/${var.region}_${var.env}_merged_output.json"
+}
+
+# Decode the JSON file
+locals {
+  merged_data                      = jsondecode(data.local_file.merged_json.content)
+  all_atms_check_contract_not_null = [for key in local.merged_data["ATM"] : merge(key, contains(keys(key), "attributes") ? {} : { "attributes" = [{ "cid" = { "source" = { "type" = "CONTEXT" }, "value" = "ClientId" } }] })]
+  all_atms_check_contract_not_null_EXPRESSION                   = [for key in local.all_atms_check_contract_not_null : contains(keys(key), "attributes") ? key["attributes"] : []]
+  all_atms_check_contract_not_null_attributeContractFulfillment = [for key in local.all_atms_check_contract_not_null : contains(keys(key), "attributes") ? key["attributes"] : []]
+  # Convert the list to a map using 'pfname' as the key
+  all_atms_check_contract_not_null_map = {
+    for atm in local.all_atms_check_contract_not_null : atm.pfname => atm
+  }
+
+  ldap_attribute_source_for_machine = {
+    ldap_attribute_source = {
+      base_dn        = var.machine_base_dn_value
+      search_scope   = "SUBTREE"
+      search_filter  = "cid=$${client_id}"
+      id             = "LdapMachine"
+      data_store_ref = { id = var.PINGFEDERATE_LDAP_DATASTORE }
+      search_attributes = [
+        "Subject DN",
+        "clm"
+      ]
+      description = "Used to get M2M claim from PingDirectory"
+      type        = "LDAP"
     }
   }
-}
 
-
-provider "external" {}
-
-data "external" "yaml_data" {
-  program = [
-    "python",
-    "${path.module}/scripts/read_yaml.py",
-    "--env", var.env,
-    "--file", var.yaml_file
-  ]
-}
-
-
-locals {
-
-  #Read the JSON file
-  python_payload = jsondecode(data.external.yaml_data.result.resources)
-# Add formatted client name with "tf" at the end and prepend "0oapf" to client_id
+  # Add formatted client name with "tf" at the end and prepend "0oapf" to client_id
   clients_with_formatted_fields = [
-    for client in local.python_payload : merge(
+    for client in local.merged_data["clients"] : merge(
       client,
       {
-        # Append "tf" to client.name
-        "pfname"      = format("%s%s", client.name, "_tf"),
         # Prepend "0oapf" to client_id (randomly generated or existing)
-        "client_id" = format("0oa14%s", random_string.client_ids[client.name].id)
+        "client_id" = "${format("0oa14%s", lookup(random_string.clientId, client.pfname, {}).result)}",
+        # Include generated password
+        "client_secret" = lookup(random_password.client_password, client.pfname, {}).result
       }
     )
   ]
+  scopes = local.merged_data["scopes"]
+  # scopes= [for scope in local.merged_data["scopes"] : {
+  #   name = scope.name
+  #   description = scope.description
+  # }]
 
+  pf_ATM_first_part_of_name=[
+    for manager in pingfederate_oauth_access_token_manager.jwt : {
+      id = manager.id
+      name = manager.name
+      first_part_of_name = element(split("_", manager.name), 0)
+    }
+  ]
+  PingDirectoryDefaultLdapEnv_machine = { ldap_attribute_source = merge({ data_store_ref = { id = var.PINGFEDERATE_LDAP_DATASTORE } }, local.ldap_attribute_source_for_machine.ldap_attribute_source) }
+  expression = {
+    Numeric_JWT_Date              = "@org.jose4j.jwt.NumericDate@now().getValue()"
+    JSONParser_LDAP_Get_country   = "new org.json.simple.parser.JSONParser().parse(#this.get('ds.LdapMachine.clm')).get('country')"
+    JSONParser_LDAP_Get_partnerID = "new org.json.simple.parser.JSONParser().parse(#this.get('ds.LdapMachine.clm')).get('partnerID')"
+    JSONParser_LDAP_Get_language  = "new org.json.simple.parser.JSONParser().parse(#this.get('ds.LdapMachine.clm')).get('language')"
+    JSONParser_LDAP_Get_clm       = "new org.json.simple.parser.JSONParser().parse(#this.get('ds.LdapMachine.clm'))"
+  }
 }
+
 # Generate random strings (client_ids) for clients that do not already have a ClientId
-resource "random_string" "client_ids" {
-  for_each    = { for client in local.python_payload : client.name => client }
+resource "random_string" "clientId" {
+  for_each = { for client in local.merged_data["clients"] : client.pfname => client }
 
   length  = 16    # Length of the random string
   special = false # Include special characters
@@ -53,7 +75,15 @@ resource "random_string" "client_ids" {
   lower   = true  # Include lowercase letters
   numeric = true  # Include numbers
 }
-# output "merged_data" {
-#   value = local.python_payload
-# }
 
+# Generate random passwords for each client
+resource "random_password" "client_password" {
+  for_each = { for client in local.merged_data["clients"] : client.pfname => client }
+
+  length           = 20   # Length of the generated password
+  special          = true # Include special characters
+  upper            = true # Include uppercase letters
+  lower            = true # Include lowercase letters
+  numeric          = true # Include numbers
+  override_special = "_"  # Special characters allowed
+}
--- variables.tf
+++ variables.tf
@@ -1,13 +1,63 @@
-variable "env" {
-  description = "The environment (e.g., US/Prod)"
+# Input variables for region and environment
+variable "region" {
+  description = "The region to deploy the resources (e.g., US, EU)"
   type        = string
 }
 
-variable "yaml_file" {
-  description = "The name of the YAML file"
+variable "PINGFEDERATE_ISSUER" {
+  description = "PingFedrate Issuer"
   type        = string
 }
 
 variable "PINGFEDERATE_AUD" {
-  type = string
+  description = "PingFederate AUD"
+  type        = string
+}
+
+variable "ATMTokenLifetime" {
+  description = "PingFederate AUD"
+  type        = string
+  default     = "60"
+}
+
+variable "UseCentralizedSigningKey" {
+  description = "PingFederate AUD"
+  type        = bool
+  default     = true
+}
+
+variable "env" {
+  description = "PingFederate AUD"
+  type        = string
+}
+
+variable "PINGFEDERATE_PROVIDER_HTTPS_HOST" {
+  description = "PingDirectory REST API URL"
+  type        = string
+}
+
+variable "PINGFEDERATE_PROVIDER_USERNAME" {
+  description = "PingDirectory UserName"
+}
+
+variable "PINGFEDERATE_PROVIDER_PASSWORD" {
+  description = "PingDirectory Password"
+  
+}
+
+variable "PINGFEDERATE_PROVIDER_ADMIN_API_PATH" {
+  description = "PingDirectory Admin API Path"
+  type        = string
+  
+}
+
+variable "PINGFEDERATE_LDAP_DATASTORE" {
+  description = "PingDirectory LDAP Datastore"
+  type        = string
+  
+}
+
+variable "machine_base_dn_value" {
+  description = "The base DN for the LDAP configuration"
+  type        = string
 }
